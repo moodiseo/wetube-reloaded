9f096a15ac2f8b8a7e1d50b7c078ef1a5524071b
moodiseo <moodiseo@gmail.com>
2025-09-25 23:34:18 +0900

    #5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,10 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "moodiseo",
+  loggedIn: true,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const search = (req, res) => res.send("Search");
