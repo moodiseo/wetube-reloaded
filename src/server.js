5cdd7e6681ff63e8ce2ae9050e41538cf506c628
moodiseo <moodiseo@gmail.com>
2025-09-25 22:07:16 +0900

    #5.2 Partials

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
