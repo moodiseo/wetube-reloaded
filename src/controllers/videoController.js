9ff4918c46fbe6cc7e0ee0dbe6374125862eb2fb
moodiseo <moodiseo@gmail.com>
2025-09-30 21:33:33 +0900

    #5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "moodiseo",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 56,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 56,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 56,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const search = (req, res) => res.send("Search");
